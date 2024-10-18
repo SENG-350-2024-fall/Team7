f0524c2c4a50c1db13fd930dca83352f71cbd278
Tristan Cornwell <tcornwell@uvic.ca>
2024-10-17 22:23:32 -0700

    Added text to VirtualTriageSurvey

--- mr_ed/src/VirtualTriageSurvey.js
+++ mr_ed/src/VirtualTriageSurvey.js
@@ -3,7 +3,8 @@ import React from 'react';
 const VirtualTriageSurvey = () => {
     return (
         <div>
-            <h1>VirtualTriage here</h1>
+            <h1>Welcome to the Mister Ed Virtual Triaging Service!</h1>
+            <h2>Please complete the below survey to receive your recommendation</h2> 
         </div>
     );
 };
