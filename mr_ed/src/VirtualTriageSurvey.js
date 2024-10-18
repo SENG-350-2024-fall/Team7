25908f729c9007ac0e30fd56907078fc571a92fe
Tristan <145408364+tristancornwell@users.noreply.github.com>
2024-10-17 19:38:26 -0700

    add VirtualTriageSurvey.js

---
+++ mr_ed/src/VirtualTriageSurvey.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VirtualTriageSurvey = () => {
+    return (
+        <div>
+            <h1>VirtualTriage here</h1>
+        </div>
+    );
+};
+
+export default VirtualTriageSurvey;
